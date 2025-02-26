1dbf83185e812d70a413b2e982a26e5905519f71
Kris Hall <kjh@ieee.org>
2025-02-26 09:54:05 -0500

    Initial generic HTTP impl

--- src/insilicalabs/ai/http.clj
+++ src/insilicalabs/ai/http.clj
@@ -1,7 +1,8 @@
 (ns insilicalabs.ai.http
   (:require
     [cheshire.core :as json]
-    [clj-http.client :as http]))
+    [clj-http.client :as http])
+  (:import (java.io IOException)))
 
 ; http config for http/request vs. http/post
 ;
@@ -22,25 +23,118 @@
 ;     - :url "https://api.openai.com/v1/chat/completions"
 ;     - :method :post
 ;     - :content-type :json
-;     - :socket-timeout 1000      ;; in milliseconds
-;     - :connection-timeout 1000  ;; in milliseconds
 ;     - :accept :json
 ;
-; - advanced options
+; - AI provider independent
+;     - :socket-timeout 1000      ;; in milliseconds
+;     - :connection-timeout 1000  ;; in milliseconds
+;
+; - advanced options (AI provider independent)
 ;     - :async? true
 ;         - ;; respond callback
 ;            (fn [response] (println "response is:" response))
 ;            ;; raise callback
 ;            (fn [exception] (println "exception message is: " (.getMessage exception))))
 ;         - cancel request
-;     - Re-using HttpClient between requests: https://github.com/dakrone/clj-http#re-using-httpclient-between-requests
-;     - Persistent Connections: https://github.com/dakrone/clj-http#persistent-connections
 ;     - :retry-handler ... IOExceptions are automatically retried, but can use retry-handler to control
 ;     - :debug true
-;
-; - todo
-;     - :throw-exceptions false
 
+
+; modes:
+;   - synch request/response, single response object
+;   - asynch request/response, single response object
+;   - stream: asynch, stream of response objects
+
+
+(def ^:const http-request-config-mods {:throw-exceptions false})
+
+(def ^:const http-get-config-mods {:method :get})
+
+(def ^:const http-post-config-mods {:method :post})
+
+(def ^:const fail-point-http-config :http-config)
+(def ^:const fail-point-http-request :http-request)
+
+
+;; todo: doc
+;;   - http errors are not returned as exceptions
+(defn ^:impure request
+  [& configs]
+  (let [config (apply merge http-request-config-mods configs)]
+    (if (nil? config)
+      {:success false
+       :fail-point fail-point-http-config
+       :reason "No configuration provided"}
+      (if (not (map? config))
+        {:success false
+         :fail-point fail-point-http-config
+         :reason "Configuration must be a map"}
+        (if (empty? config)
+          {:success false
+           :fail-point fail-point-http-config
+           :reason "Configuration map cannot be empty"}
+          (if (not (contains? config :method))
+            {:success false
+             :fail-point fail-point-http-config
+             :reason "Config must contain the key ':method'"}
+            (if (not (#{:get :post} (:method config)))
+              {:success false
+               :fail-point fail-point-http-config
+               :reason "The :method value must be either ':get' or ':post'"}
+              (if (not (contains? config :url))
+                {:success false
+                 :fail-point fail-point-http-config
+                 :reason "Config must contain a URL defined in ':url'"}
+                (if (not (string? (:url config)))
+                  {:success false
+                   :fail-point fail-point-http-config
+                   :reason "The value of ':url' must be a string"}
+                  (try
+                    (let [response (http/request config)]
+                      (if (http/success? response)
+                        {:success true :response response}
+                        {:success false
+                         :fail-point fail-point-http-request
+                         :reason (str "HTTP request failed. " (:reason-phrase response) " (" (:status response) ").")
+                         :response response}))
+                    (catch IOException e
+                      (let [reason-start (str "HTTP request failed with exception '" (.getName (class e)) "'.")
+                            message (str (.getMessage e))
+                            reason (if (or (nil? message) (= message ""))
+                                     reason-start
+                                     (str reason-start " " message "."))]
+                        (println message)
+                        {:success false
+                         :fail-point fail-point-http-request
+                         :reason reason
+                         :exception e}))))))))))))
+;; java.net.MalformedURLException
+;; java.net.UnknownHostException
+;; ...
+;; https://download.java.net/java/early_access/panama/docs/api/java.base/java/net/package-summary.html
+
+;; if 'response' in err, then have ':status' and ':reason-phrase'
+
+;; test status codes:
+;; https://httpstat.us/
+
+;; success     <boolean>
+;; fail-point  <key, e.g. ':http-request'>
+;; reason      <key or string?>
+;;
+;; response    <response map>
+;; exception   <exception>
+
+
+;; todo docs / simple usage. reference docs in 'request'.
+(defn ^:impure get
+  [& configs]
+  (request (apply merge http-get-config-mods configs)))
+
+;; todo docs / simple usage. reference docs in 'request'.
+(defn ^:impure post
+  [& configs]
+  (request (apply merge http-post-config-mods configs)))
 
 
 ;(defn- complete-impl [config context]
