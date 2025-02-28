fd5aa3d077ae0fde1c0c0f2a638285738d5a93b4
Kris Hall <kjh@ieee.org>
2025-02-28 13:20:57 -0500

    create example usage driver

---
+++ src/insilicalabs/ai/example.clj
@@ -0,0 +1,68 @@
+(ns insilicalabs.ai.example
+  (:require [clojure.java.io :as io]
+            [insilicalabs.ai.providers.openai :as ai]))
+
+;; USAGE:
+;;   (require '[insilicalabs.ai.example :as example] :reload)
+
+
+(defn get-api-key
+  []
+  (println "Enter the path to the file containing the API key, or enter \"0\" to exit:")
+  (let [path (clojure.string/trim (read-line))]
+    (cond
+      (= path "0") (println "Exiting...")
+      (not (.exists (io/file path))) (do
+                                       (println "Error: File does not exist. Please enter a valid path.")
+                                       (recur))
+      :else (let [api-key (clojure.string/trim (slurp path))]
+              api-key))))
+
+
+(defn complete-synch
+  [config]
+  (println "Enter text to send for a synchronous completion, or enter \"0\" to exit.")
+  (loop []
+    (println "")
+    (println "------------------------------------")
+    (let [choice (clojure.string/trim (read-line))]
+      (cond
+        (= choice "0") (println "Leaving 'completion, synchronous'")
+        :else (do
+                (println ">> " (ai/complete config choice))
+                (println "")
+                (recur))))))
+
+
+(defn bravo
+  [config]
+  (println "You selected bravo."))
+
+
+(defn charlie
+  [config]
+  (println "You selected charlie."))
+
+
+(let [api-key (get-api-key)]
+  (if (some? api-key)
+    (loop []
+      (println "Select an option:")
+      (println "(1) complete, synchronous")
+      (println "(2) bravo")
+      (println "(3) charlie")
+      (println "(0) EXIT")
+      (let [config {:api-key api-key}
+            choice (clojure.string/trim (read-line))]
+        (case choice
+          "1" (do (complete-synch config) (recur))
+          "2" (do (bravo config) (recur))
+          "3" (do (charlie config) (recur))
+          "0" (println "bye")
+          (do (println "Error: Invalid choice, please select again.")
+              (recur)))))))
+
+
+
+
+
--- src/insilicalabs/ai/providers/openai.clj
+++ src/insilicalabs/ai/providers/openai.clj
@@ -4,6 +4,12 @@
     [insilicalabs.ai.http :as http]
     [insilicalabs.ai.providers.sse-stream :as sse-stream]))
 
+
+(def ^:const default-complete-model "gpt-4o")
+
+(def ^:const default-completions-url "https://api.openai.com/v1/chat/completions")
+
+
 (defn- create-context [context-or-text]
   (cond
     (nil? context-or-text) []
@@ -15,13 +21,12 @@
   (let [stream (get config :stream false)
         response (http/post
                    (cond->
-                     {:url              "https://api.openai.com/v1/chat/completions"
-                      ;:debug            true
+                     {:url              default-completions-url
                       :throw-exceptions false
                       :content-type     :json
-                      :headers          {"Authorization" (str "Bearer " (:openai-api-key config))}
+                      :headers          {"Authorization" (str "Bearer " (:api-key config))}
                       :body             (json/generate-string
-                                          {:model    "gpt-4o"
+                                          {:model    default-complete-model
                                            :stream   stream
                                            :messages context})}
                      stream (assoc :as :reader)))]
@@ -46,18 +51,18 @@
    {:role \"assistant\" :content \"Here are some ice cream flavors I recommend ...\"}
    {:role \"user\" :content \"Great! Thank you. Now can you ...\"}]
 
-  config must be a map and contain :openai-api-key with a string of your OpenAI api key.
+  config must be a map and contain :api-key with a string of your OpenAI api key.
 
   The three arg form takes the context and a new user message to add to the context.
 
-  config - a map which must contain :openai-api-key
+  config - a map which must contain :api-key
   context - a vector of messages of the form [{:role \"user\" :content \"message...\"}]
   context-or-text a vector of messages or a string message from user
   user-message - a string with a new user message to add to the context"
   ([config context user-message]
    (complete config (conj context {:role "user" :content user-message})))
   ([config context-or-text]
-   {:pre [(:openai-api-key config)]}
+   {:pre [(:api-key config)]}
    (let [context (create-context context-or-text)]
      (complete-impl config context))))
 
